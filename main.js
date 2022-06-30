2b7db028cf93ccc072f5c2ded743a945047e1d0e
unknown <wesleyberto4@gmail.com>
2022-06-30 19:30:45 -0300

    project calculato

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 
-const selector = document.getElementById("select")
-let resultado = document.getElementById('resultado')
-const btn = document.querySelector("button")
+let select = document.getElementById("select")
+let resultado = document.getElementById("resultado")
+let btn = document.querySelector("button")
 
-btn.addEventListener("click" , () =>{
-    let n1 = Namber(document.getElementById("num1").value)
-    let n2 = Namber(document.getElementById("num2").value)
-    let option = selector.options[selector.selectedIndex].value
-    if( option == '+'){
-        resultado.value = n1 + n2 
-    }else if ( option == '-' ){
-        resultado.value = n1 - n2
+btn.addEventListener("click" , () => {
+    let num1 = Number(document.getElementById("num1").value)
+    let num2 = Number(document.getElementById("num2").value)
+    let option = select.options[select.selectedIndex].value
+    if( option =="+"){
+        resultado.value = num1 + num2 
+    }else if ( option == "-" ){
+        resultado.value = num1 - num2
+
     }else if( option.value == "*"){
-        resultado.value = n1 * n2
+        resultado.value = num1 * num2
+
     }else if ( option.value == "/"){
-        resultado.value = n1 / n2
+        resultado.value = num1 / num2
     }
     else{
         alert("selecione uma opção!")
     }
-
-
 })
